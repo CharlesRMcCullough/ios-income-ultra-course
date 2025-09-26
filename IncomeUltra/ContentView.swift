478b0169a6b3946983f761f31e3f95bf275e42c8
Charles McCullough <charles.mccullough@gmail.com>
2025-09-25 21:22:07 -0500

    Initial Commit

---
+++ IncomeUltra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IncomeUltra
+//
+//  Created by Charles McCullough on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IncomeUltra/IncomeUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltraApp.swift
+//  IncomeUltra
+//
+//  Created by Charles McCullough on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
