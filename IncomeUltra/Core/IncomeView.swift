5e26cdcec664caa062bd65193b3ec21d3cebd622
Charles McCullough <charles.mccullough@gmail.com>
2025-09-27 12:10:17 -0500

    HomeTabView

---
+++ IncomeUltra/Core/IncomeView.swift
@@ -0,0 +1,18 @@
+//
+//  IncomeView.swift
+//  IncomeUltra
+//
+//  Created by Charles McCullough on 9/27/25.
+//
+
+import SwiftUI
+
+struct IncomeView: View {
+    var body: some View {
+        Text("Income View")
+    }
+}
+
+#Preview {
+    IncomeView()
+}
---
+++ IncomeUltra/Core/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  IncomeUltra
+//
+//  Created by Charles McCullough on 9/27/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
